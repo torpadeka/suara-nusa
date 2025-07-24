57676d58c6ebcfc6ae1304d5797f97ca882d0ee4
Darren Kent <darrenkent88@gmail.com>
2025-07-24 15:53:19 +0700

    Adjusted firebase setup and testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Cities from "./Cities";
 
 export default function Home() {
     return (
@@ -50,6 +51,7 @@ export default function Home() {
                         Read our docs
                     </a>
                 </div>
+                <Cities />
             </main>
             <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
                 <a
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-import "dotenv/config";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "suara-nusa.firebaseapp.com",
-    projectId: "suara-nusa",
-    storageBucket: "suara-nusa.firebasestorage.app",
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: "G-E973SW09N7",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
